788420adad418c82109bbe79b9cab8d096db6aa3
Guillermo Brum <guillermobrum@hotmail.com>
2023-01-31 22:09:45 -0300

    proyectoFinal

---
+++ curri.js
@@ -0,0 +1,9 @@
+
+//document.getElementById('boton').addEventListener('click', function() {
+//console.log("funciona esta wea?");
+//});
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("nightmode");
+ }
