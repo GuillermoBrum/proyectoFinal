d4925dcd5a03095192c011a0e8a1b55fffa9c210
Guillermo Brum <guillermobrum@hotmail.com>
2023-01-31 23:34:52 -0300

    cambios de archivos

--- curri.js
+++ curri.js
@@ -1,8 +1,4 @@
 
-//document.getElementById('boton').addEventListener('click', function() {
-//console.log("funciona esta wea?");
-//});
-
 function myFunction() {
     var element = document.body;
     element.classList.toggle("nightmode");
