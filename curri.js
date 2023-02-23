b386d76f7d3e5a62023ba1c4f867bd41f78175ca
Guillermo Brum <guillermobrum@hotmail.com>
2023-02-23 15:08:41 -0300

    modificacion de css

--- curri.js
+++ curri.js
@@ -1,5 +1,14 @@
 
 function myFunction() {
-    var element = document.body;
+    let element = document.body;
     element.classList.toggle("nightmode");
  }
+
+ function topNav() {
+    let x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
